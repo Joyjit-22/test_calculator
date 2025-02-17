61b0b138b8231fb134aa2a79d58037758115ba3e
Joyjit <joyjitpc@gmail.com>
2025-02-17 21:49:15 +0530

    adding mul feature

--- calculator.py
+++ calculator.py
@@ -4,6 +4,10 @@ def pow(a,b):
     return a**b
 def div(a,b):
     return a-b
+def mul(a,b):
+    return a*b
 __name__ = "__main__"
 print(add(2,3))
 print(div(2,3))
+print(pow(2,3))
+print(mul(2,3))
