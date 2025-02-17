4081cd12608721e21f86e275b4fa22120bae34aa
unknown-spec10 <deeppodder57@gmail.com>
2025-02-17 21:05:59 +0530

    Deep commit

--- calculator.py
+++ calculator.py
@@ -2,3 +2,10 @@ def add_two_number(a,b):
     return a+b
 
 print(add_two_number(2,4))
+
+def divide(a,b):
+    return a/b
+
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number: "))
+print(divide(a/b))
